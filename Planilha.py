c1d88cbcbb5bea36193c561662d84bfc37b476ad
Jp-Vianna <jpnardyvianna@gmail.com>
2024-06-19 22:40:21 -0300

    Squashed commit of the following:
    commit 89813a7a1c1bc0ade5698018ed1e947ec3c81f71
    Author: Jp-Vianna <jpnardyvianna@gmail.com>
    Date:   Wed Jun 19 22:35:31 2024 -0300
    
        Funções de edição e exclusão adicionadas.

--- Planilha.py
+++ Planilha.py
@@ -9,8 +9,49 @@ class Planilha:
         # Seleciona página a ser trabalhada.
         self.pagina_despezas = self.book["Despezas"]
 
-    def add(self, valor, titulo, tipo):
+    def adiciona_linha(self, valor, titulo, tipo):
         # Adiciona nova linha.
         self.pagina_despezas.append([valor, titulo, tipo])
         # Salva mudanças do arquivo.
         self.book.save('PlanilhaDespezas.xlsx')
+
+    # for linha in self.planilha.pagina_despezas.iter_rows(min_row=2):
+    #     if any(celulas.value is None for celulas in linha[:3]):
+    #         break
+
+    def remove(self, despeza):
+        contador = 1
+        for linha in self.pagina_despezas.iter_rows(min_row=2):
+            contador += 1
+
+            if self.verifica_vazio(linha):
+                return
+
+            if self.verifica_igualdade(linha, despeza.valor, despeza.titulo, despeza.tipo):
+                self.pagina_despezas.delete_rows(contador, 1)
+                self.book.save("PlanilhaDespezas.xlsx")
+                break
+
+    def edita(self, despeza, valores):
+        for linha in self.pagina_despezas.iter_rows(min_row=2):
+            print(self.verifica_vazio(linha))
+            if self.verifica_vazio(linha):
+                return
+
+            print(self.verifica_igualdade(linha, valores[0], valores[1], valores[2]))
+            if self.verifica_igualdade(linha, valores[0], valores[1], valores[2]):
+                linha[0].value = f"R$ {despeza.valor:.2f}"
+                linha[1].value = despeza.titulo
+                linha[2].value = despeza.tipo
+                self.book.save("PlanilhaDespezas.xlsx")
+                break
+
+    @staticmethod
+    def verifica_igualdade(linha, valor, titulo, tipo):
+        return (float(linha[0].value[3:]) == valor) and (linha[1].value == titulo) and (linha[2].value == tipo)
+
+    @staticmethod
+    def verifica_vazio(linha):
+        return any(celulas.value is None for celulas in linha[:3])
+
+
